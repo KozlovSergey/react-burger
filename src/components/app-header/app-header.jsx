eb8590483277b6b164218bca964d618e4a013841
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-01-26 12:17:36 +0400

    Fill app header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import NavLink from "../nav-link/nav-link";
-import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./app-header.module.css";
 
 class AppHeader extends React.Component {
   render() {
     return (
       <header className="pt-4 pb-4">
-        <nav>
-          <NavLink title="Конструктор" icon={<BurgerIcon type="primary" />} />
+        <nav className={styles.nav}>
+          <div className={`${styles.navItem}`}>
+            <NavLink title="Конструктор" icon={<BurgerIcon type="primary"/>}/>
+            <NavLink title="Лента заказов" icon={<ListIcon type="primary"/>}/>
+          </div>
+          <div className={styles.navItem}>
+            <Logo/>
+          </div>
+          <div className={styles.navItem}>
+            <NavLink title="Личный кабинет" icon={<ProfileIcon type="primary"/>}/>
+          </div>
         </nav>
       </header>
     );
