16a8217a60072f1dd62a9f00265c96e3f5fb7988
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-01-27 18:25:59 +0400

    fix class for EmptySpace

--- src/components/empty-space/empty-space.jsx
+++ src/components/empty-space/empty-space.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import classNames from 'classnames';
 import styles from './empty-space.module.css';
 
 class EmptySpace extends React.Component {
   render(props) {
-    const heightClassName = 'space-' + this.props.height;
-    console.log(heightClassName);
+    const heightClassName = `space${this.props.height}`;
     
     return (
-      <div
-        className={classNames('space', {
-          [`${styles.space + this.props.height}`]: true
-        })}
-      ></div>
+      <div className={styles[heightClassName]}></div>
     )
   }
 }
