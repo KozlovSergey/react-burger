8e957b36db0842c9130abb6eea94e49b7f90cfbf
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-01-27 20:58:13 +0400

    add BurgerIngredientItem

---
+++ src/components/burger-ingridient-item/burger-ingridient-item.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import EmptySpace from "../empty-space/empty-space";
+import styles from './burger-ingridient-item.module.css';
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+class BurgerIngredientItem extends React.Component {
+  render() {
+    return (
+      <div className={styles.item}>
+        <div className="image">
+          <img src="https://code.s3.yandex.net/react/code/bun-02.png"/>
+        </div>
+        <EmptySpace height="X1"/>
+        <div className={styles.price}>
+          <span className={`${styles.priceValue} text text_type_digits-default`}>20</span>
+          <span className="text text_type_digits-default"><CurrencyIcon type="primary" /></span>
+        </div>
+        <EmptySpace height="X1"/>
+        <div className="name">Краторная булка N-200i</div>
+      </div>
+    )
+  }
+}
+
+export default BurgerIngredientItem;
