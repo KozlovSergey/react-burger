e98c5242969a88413966cca5a3cf0e0007a28038
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-01-27 18:38:32 +0400

    integrate BurgerIngredientsTabs

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import EmptySpace from "../empty-space/empty-space";
+import BurgerIngredientsTabs from "../burger-ingridients-tabs/burger-ingridients-tabs";
 
 class BurgerIngredients extends React.Component {
   render() {
     return (
-      <div>burger-ingredients</div>
+      <section>
+        <EmptySpace height="X10"/>
+        <h1 className="text text_type_main-large">
+          Соберите бургер
+        </h1>
+        <EmptySpace height="X5"/>
+        <BurgerIngredientsTabs/>
+        <EmptySpace height="X10"/>
+        <h2 className="text text_type_main-medium">
+          Булки
+        </h2>
+      </section>
     )
   }
 }
