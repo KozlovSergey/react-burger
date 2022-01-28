0d6f3bf3d73d286e32037fb4d079d2b5d3ebbdfc
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-01-28 18:10:48 +0400

    update ingredients scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -6,7 +6,7 @@ import styles from "./burger-ingredients.module.css";
 
 class BurgerIngredients extends React.Component {
   render(props) {
-    const { data } = this.props;
+    const {data} = this.props;
     let buns = data.filter(item => item.type === 'bun');
     let sauces = data.filter(item => item.type === 'sauce');
     let mains = data.filter(item => item.type === 'main');
@@ -19,29 +19,31 @@ class BurgerIngredients extends React.Component {
         <EmptySpace height="X5"/>
         <BurgerIngredientsTabs/>
         <EmptySpace height="X10"/>
-        <h2 className="text text_type_main-medium">
-          Булки
-        </h2>
-        <div className={styles.burger_ingredients_wrapper}>
-          {buns.map(item => (
-            <BurgerIngredientItem itemData={item}/>
-          ))}
-        </div>
-        <h2 className="text text_type_main-medium">
-          Соусы
-        </h2>
-        <div className={styles.burger_ingredients_wrapper}>
-          {sauces.map(item => (
-            <BurgerIngredientItem itemData={item}/>
-          ))}
-        </div>
-        <h2 className="text text_type_main-medium">
-          Начинки
-        </h2>
-        <div className={styles.burger_ingredients_wrapper}>
-          {mains.map(item => (
-            <BurgerIngredientItem itemData={item}/>
-          ))}
+        <div className={styles.wrapper}>
+          <h2 className="text text_type_main-medium">
+            Булки
+          </h2>
+          <div className={styles.burger_ingredients_wrapper}>
+            {buns.map(item => (
+              <BurgerIngredientItem itemData={item}/>
+            ))}
+          </div>
+          <h2 className="text text_type_main-medium">
+            Соусы
+          </h2>
+          <div className={styles.burger_ingredients_wrapper}>
+            {sauces.map(item => (
+              <BurgerIngredientItem itemData={item}/>
+            ))}
+          </div>
+          <h2 className="text text_type_main-medium">
+            Начинки
+          </h2>
+          <div className={styles.burger_ingredients_wrapper}>
+            {mains.map(item => (
+              <BurgerIngredientItem itemData={item}/>
+            ))}
+          </div>
         </div>
       </section>
     )
