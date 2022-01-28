ebd215577739258cf0e6e04823b6beec9d6b6bc3
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-01-28 16:21:30 +0400

    add data

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,14 +3,15 @@ import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import styles from './app.module.css';
+import data from '../../utils/data';
 
 function App() {
   return (
     <div className={styles.app}>
       <AppHeader/>
       <main className={styles.main}>
-        <BurgerIngredients/>
-        <BurgerConstructor/>
+        <BurgerIngredients props={data} />
+        <BurgerConstructor props={data} />
       </main>
     </div>
   );
