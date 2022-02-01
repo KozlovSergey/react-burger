fbd96a93f704e52e859a9cdf5cada5e555bc91b9
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-02-01 22:22:04 +0400

    add api data

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import styles from './app.module.css';
-import data from '../../utils/data';
+import { API_GET_DATA } from "../../utils/constants";
 
 function App() {
+  const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    fetch(API_GET_DATA)
+      .then(result => {
+        if (result.ok) {
+          return result.json();
+        }
+        return Promise.reject(`Ошибка ${result.status}`);
+      })
+      .then(data => setData(data.data))
+      .catch(e => {
+        console.log('Error: ' + e.message);
+      });
+  }, []);
+
   return (
     <div className={styles.app}>
       <AppHeader/>
