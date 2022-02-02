c2f2fe57df199e06ec9804ca9b72d54c591d2213
Sergey Kozlov <sergey.kozlov@icertainty.com>
2022-02-02 16:40:03 +0400

    add modal-overlay component

---
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import styles from './modal-overlay.module.css';
+
+const modalRoot = document.getElementById("modals");
+
+const ModalOverlay = (props) => {
+  return ReactDOM.createPortal(
+    (
+      <div className={styles.root} onClick={props.onClick}>
+        {props.children}
+      </div>
+    ),
+    modalRoot
+  );
+};
+
+ModalOverlay.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+export default ModalOverlay;
